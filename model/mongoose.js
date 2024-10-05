c108cec424eec9b78fc36285c3050c3ffae74f43
sandeep <sandeepcheriyan@gmail.com>
2024-10-05 19:29:12 +0530

    mngo atlas connected

--- model/mongoose.js
+++ model/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const link = 'mongodb://127.0.0.1:27017/machinTask2?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1';
+// const link = 'mongodb://127.0.0.1:27017/machinTask2?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1';
+const link ="mongodb+srv://sandeeppachat:w6yGtOSj60IeUvXk@cluster0.s4hqvyg.mongodb.net/machinTask2?retryWrites=true&w=majority"
 mongoose.connect(link)
 .then(() => {
     console.log('MongoDB Connected');
